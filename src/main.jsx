685d3bd32c0c7d7ad4ad1936a91a58647b26c6d0
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-03-20 11:12:15 +0100

    added basic router

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,25 @@
+//base
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+//for redux
 import store from './redux/store'
 import {Provider} from 'react-redux'
+//for react router
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 )
