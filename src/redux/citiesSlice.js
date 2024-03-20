fde7ffa585df31d30f874aed18c83276bf41041a
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-03-20 11:09:15 +0100

    added and implemented redux store

---
+++ src/redux/citiesSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const citiesSlice = createSlice({
+    name: 'cities',
+    initialState: [
+        
+    ],
+    reducers: {
+        
+    }
+})
+
+export default citiesSlice.reducer
