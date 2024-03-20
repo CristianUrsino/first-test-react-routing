1102e2a8a909f9b736fea2620c71c3f00d935535
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-03-20 11:09:35 +0100

    updated citiesSlice

--- src/redux/citiesSlice.js
+++ src/redux/citiesSlice.js
@@ -3,11 +3,41 @@ import { createSlice } from "@reduxjs/toolkit";
 export const citiesSlice = createSlice({
     name: 'cities',
     initialState: [
-        
+        {
+            name: 'Roma',
+            country: 'Italia',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quod.',
+            image: 'https://picsum.photos/200/300',
+            visited: false
+        },
+        {
+            name: 'Milan',
+            country: 'Italia',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quod.',
+            image: 'https://picsum.photos/200/300',
+            visited: true
+        },
+        {
+            name: 'Napoli',
+            country: 'Italia',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quod.',
+            image: 'https://picsum.photos/200/300',
+            visited: false
+        },
+        {
+            name: 'Palermo',
+            country: 'Italia',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quod.',
+            image: 'https://picsum.photos/200/300',
+            visited: true
+        }
     ],
     reducers: {
-        
+        addCity: (state, action) => {
+            state.push(action.payload)
+        }
     }
 })
 
+export const { addCity } = citiesSlice.actions
 export default citiesSlice.reducer
