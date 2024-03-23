15db1ca9927e93093c82444d90113c87a67ce5f9
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-03-23 10:28:46 +0100

    updated App with layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+//hooks
 import { useState } from 'react'
+//styles
 import './App.css'
-
+//components
+import NavbarComponent from './components/NavbarComponent.jsx'
+import FooterComponent from './components/FooterComponent.jsx'
+//routing
+import { Outlet } from 'react-router-dom'
 function App() {
 
   return (
     <>
-    <div>app</div>
+    <NavbarComponent />
+    <div>
+      <Outlet />
+    </div>
+    <FooterComponent />
     </>
   )
 }
